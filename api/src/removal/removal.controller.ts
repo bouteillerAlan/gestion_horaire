50d6544627839c8ac9b1f297950cabd3091d7884
Alan Bouteiller <bouteiller.alan@gmail.com>
2020-02-01 17:36:19 +0100

    :sparkles: feat(removal): add crud

---
+++ api/src/removal/removal.controller.ts
@@ -0,0 +1,29 @@
+import {Body, Param, Controller, Delete, Get, Post, Put} from '@nestjs/common';
+import {RemovalService} from './removal.service';
+import {RemovalDto} from './removal.dto';
+import {RemovalEditDto} from './removal.edit.dto';
+
+@Controller('removal')
+export class RemovalController {
+  constructor(private readonly removalService: RemovalService) {}
+
+  @Get()
+  async getAll() {
+    return this.removalService.findAll();
+  }
+
+  @Post()
+  async insert(@Body() data: RemovalDto) {
+    return this.removalService.insert(data);
+  }
+
+  @Put('/:id')
+  async update(@Body() data: RemovalEditDto, @Param() id: number) {
+    return this.removalService.update(id, data);
+  }
+
+  @Delete('/:id')
+  async delete(@Param() id: number) {
+    return this.removalService.delete(id);
+  }
+}
---
+++ api/src/removal/removal.service.ts
@@ -0,0 +1,39 @@
+import {BadRequestException, Injectable} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
+import {Removal} from './removal.entity';
+import {RemovalDto} from './removal.dto';
+import {RemovalEditDto} from './removal.edit.dto';
+
+@Injectable()
+export class RemovalService {
+  constructor(
+    @InjectRepository(Removal)
+    private readonly removalRepository: Repository<Removal>,
+  ) {}
+
+  async findAll(): Promise<Removal[]> {
+    return this.removalRepository.find();
+  }
+
+  async insert(data: RemovalDto): Promise<any> {
+    return this.removalRepository.insert(data);
+  }
+
+  async update(id: number, data: RemovalEditDto): Promise<any> {
+    const testId: any[] = await this.removalRepository.findByIds([id]);
+    if (testId.length === 0) {
+      throw new BadRequestException('Cet id n\'existe pas');
+    }
+    return this.removalRepository.update(id, data);
+  }
+
+  async delete(id: number): Promise<any> {
+    const result: any = await this.removalRepository.delete(id);
+    if (result.affected !== 1) {
+      throw new BadRequestException('Cet id n\'existe pas');
+    }
+    return result;
+  }
+
+}
