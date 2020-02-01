40688b91ef19d57e5863c6d4cb8bde07b8a71fde
Alan Bouteiller <bouteiller.alan@gmail.com>
2020-02-01 18:58:07 +0100

    :wrench: build(next): improve less loader for antd

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,10 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
+const withLess = require('@zeit/next-less');
 
-module.exports = withCSS(withSass({
-  // config
-}));
+module.exports = withSass(withCSS(withLess({
+  // cssModules: true,
+  lessLoaderOptions: {
+    javascriptEnabled: true
+  }
+})));
